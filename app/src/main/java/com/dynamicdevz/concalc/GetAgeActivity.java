953fdb433454a14dc2ba07ab148ec18cb41ea0a4
Dalo-Chinkhwangwa-Prof <Dalo.chinkhwangwa@outlook.com>
2021-06-17 10:54:51 -0400

    SharedPreference + onActivityResult

--- app/src/main/java/com/dynamicdevz/concalc/GetAgeActivity.java
+++ app/src/main/java/com/dynamicdevz/concalc/GetAgeActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class GetAgeActivity extends AppCompatActivity {
 
-
+    public static final String TAG = "TAG_X";
     private TextView personTV;
 
     @Override
@@ -19,11 +25,42 @@ public class GetAgeActivity extends AppCompatActivity {
         personTV = findViewById(R.id.textView2);
 
         Intent rI = getIntent();
+        Log.d(TAG, "onCreate: 2nd");
         if(rI != null){
             String name = rI.getStringExtra("name");
             int yob = rI.getIntExtra("yob", 2000);
-            personTV.setText(name + " age is "+ (2021 - yob));
-        }
 
+            Date date = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy", Locale.US);
+            int dateYear = Integer.parseInt(dateFormat.format(date));
+
+            Log.d(TAG, "Date year "+ dateYear);
+            String fromActivity = name + " age is "+ (dateYear - yob);
+            personTV.setText(fromActivity);
+
+            Intent dataIntent = new Intent();
+            dataIntent.putExtra("from", fromActivity.toUpperCase());
+            setResult(RESULT_OK, dataIntent);
+
+//            new Handler(getMainLooper()).postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+                    Log.d(TAG, "closing");
+                    finish();//Closes current activity
+//                }
+//            }, 3000);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: 2nd");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: 2nd");
     }
 }
--- app/src/main/java/com/dynamicdevz/concalc/MainActivity.java
+++ app/src/main/java/com/dynamicdevz/concalc/MainActivity.java
@@ -1,25 +1,50 @@
 package com.dynamicdevz.concalc;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText nameEditText;
     private EditText yearOBEditText;
+    public static final String TAG = "TAG_X";
+    private final int REQUEST_CODE = 797;
 
+
+    private SharedPreferences sharedPreferences;
+    private TextView tv;
     //Intents: How android performs it's actions
     //Implicit Intent
     //Explicit Intent
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alt_home_activity);
+        tv = findViewById(R.id.my_text);
+        sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+        String value = sharedPreferences.getString("KEY", null);
+        if(value != null){
+            tv.setVisibility(View.VISIBLE);
+            tv.setText(value);
+        }
+
 
         nameEditText = findViewById(R.id.firstname_edittext);
         yearOBEditText = findViewById(R.id.yob_edittext);
@@ -33,11 +58,24 @@ public class MainActivity extends AppCompatActivity {
 
                 getAgeIntent.putExtra("yob", Integer.parseInt(yearOBEditText.getText().toString()));
                 getAgeIntent.putExtra("name", nameEditText.getText().toString().trim());
-
-
+                startActivityForResult(getAgeIntent, REQUEST_CODE);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: ");
+        if(requestCode == REQUEST_CODE){
+            String dataString = data.getStringExtra("from");
+            Toast.makeText(this, dataString, Toast.LENGTH_LONG).show();
+
+            SharedPreferences.Editor edit = sharedPreferences.edit();
+            edit.putString("KEY", dataString);
+            edit.apply();
+            //class Human(String name, int age)
+        }
     }
 }
 
